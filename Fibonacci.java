10ae095411dbe66a70f21196aed1b38f12670cba
Vikas Shettigar <thewecas@gmail.com>
2023-02-08 00:03:51 +0530

    ,,

--- Fibonacci.java
+++ Fibonacci.java
@@ -12,7 +12,7 @@ public class Fibonacci{
     public static Long fib(int n){
         if (n<=2)                   return (long)1;         //base case
         if (memo.containsKey(n))    return memo.get(n);     //return fib(n) from memo if it's already calculated &  present
-        memo.put(n,fib(n-1) + fib(n-2));                    //if not present, calculate it & add to memo
+        memo.put(n,fib(n-1) + fib(n-2));                    //if not present, calculate it & add to memo 
         return memo.get(n);
     }
 
